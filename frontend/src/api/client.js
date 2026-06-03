ae4b48e723454b699c31df659f3db1aedd154306
Ravleen Kaur <rkaur161205@gmail.com>
2026-06-04 01:56:37 +0530

    edit file

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000",
+  baseURL: import.meta.env.VITE_API_URL || "https://failsafe-backend.onrender.com",
 });
 
 client.interceptors.request.use((config) => {
